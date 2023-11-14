3c5b4d5a1924343894ec9fb9d5634e851b68f391
git-create-devben <DevBen.tech@outlook.com>
2023-11-14 14:25:58 +0100

    create the usercircle

---
+++ app/components/threadcircle.tsx
@@ -0,0 +1,44 @@
+// components/ThreadCircle.tsx
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Thread {
+  threadId: string;
+  // Add other properties based on the API response
+}
+
+interface ThreadCircleProps {
+  username: string;
+}
+
+const ThreadCircle: React.FC<ThreadCircleProps> = ({ username }) => {
+  const [threads, setThreads] = useState<Thread[]>([]);
+
+  useEffect(() => {
+    const fetchUserThreads = async () => {
+      try {
+        const response = await axios.get(`/rapidapi/users/threads?username=${username}`);
+        setThreads(response.data.threads);
+      } catch (error) {
+        console.error('Error fetching user threads:', error);
+      }
+    };
+
+    fetchUserThreads();
+  }, [username]);
+
+  return (
+    <div>
+      <h2>Thread Circle for {username}</h2>
+      {/* Your thread circle visualization goes here */}
+      <ul>
+        {threads.map((thread) => (
+          <li key={thread.threadId}>{thread.threadId}</li>
+          {/* Customize the display based on the properties you want to show */}
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ThreadCircle;
