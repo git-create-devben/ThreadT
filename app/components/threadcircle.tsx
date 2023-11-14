03ae6d0daa370ee45fc2d03b1b88a679287dcc7a
git-create-devben <DevBen.tech@outlook.com>
2023-11-14 14:50:13 +0100

    add some design

--- app/components/threadcircle.tsx
+++ app/components/threadcircle.tsx
@@ -1,6 +1,6 @@
 // components/ThreadCircle.tsx
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 interface Thread {
   threadId: string;
@@ -13,6 +13,7 @@ interface ThreadCircleProps {
 
 const ThreadCircle: React.FC<ThreadCircleProps> = ({ username }) => {
   const [threads, setThreads] = useState<Thread[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchUserThreads = async () => {
@@ -20,17 +21,25 @@ const ThreadCircle: React.FC<ThreadCircleProps> = ({ username }) => {
         const response = await axios.get(`/rapidapi/users/threads?username=${username}`);
         setThreads(response.data.threads);
       } catch (error) {
-        console.error('Error fetching user threads:', error);
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+          setError(`Error: ${axiosError.message}, Status: ${axiosError.response?.status}`);
+        } else {
+          setError('An unexpected error occurred.');
+        }
       }
     };
 
     fetchUserThreads();
-  },);
+  }, [username]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div className='text-white'>
       <h2>Thread Circle for {username}</h2>
-      {/* Your thread circle visualization goes here */}
       <ul>
         {threads.map((thread) => (
           <li key={thread.threadId}>{thread.threadId}</li>
